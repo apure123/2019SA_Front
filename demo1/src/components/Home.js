4fa808b16bfa0c1ae96757928d4cd39a44b33e38
Apure <1007720616@qq.com>
2019-06-05 14:51:04 +0800

    展示前最终版

--- demo1/src/components/Home.js
+++ demo1/src/components/Home.js
@@ -13,8 +13,8 @@ class Home extends Component{
     constructor(props) {
         super(props);
         /*this.props.init();*/
-        this.get_recom1();
-        this.get_recom2()
+      /*  this.get_recom1();
+        this.get_recom2()*/
 
     }
 
@@ -78,7 +78,34 @@ class Home extends Component{
                     <Col span={12}>
                         <Card title="学术动态" bordered={false}>
 
-                            {
+                            <Link to={`/system/article?uid=ba848e02-267b-4459-9cea-eaaaab423f82=P1`} >
+                                <p>磁控形状记忆合金Ni2MnGa的制备与性能研究</p>
+                            </Link>
+
+                            <Link to={`/system/article?uid=9567dd69-ab95-486f-88fb-6a58be440b0a=P1`} >
+                                <p>特种功能材料中的固态相变及应用</p>
+                            </Link>
+                            <Link to={`/system/article?uid=03d0d191-af1e-4962-a45c-7608fd9249e9=P1`} >
+                                <p>基板温度对EB-PVD梯度热障涂层的微观结构和性能的影响</p>
+                            </Link>
+
+                            <Link to={`/system/article?uid=14fadd5b-15e9-4c5b-b889-99ed52adeda3=P1`} >
+                                <p>TiNiMo形状记忆合金的相变、形状记忆效应与力学性能研究</p>
+                            </Link>
+                            <Link to={`/system/article?uid=a6f5b6e9-6cf2-47ab-b161-5d624365fab7=P1`} >
+                                <p>乌克兰巴顿焊接研究所的电子束物理气相沉积技术 </p>
+                            </Link>
+
+                            <Link to={`/system/article?uid=8f2576de-43e1-4d71-836f-797037e50c89=P1`} >
+                                <p>表面强化对EB-PVD热障涂层的高温氧化性能及结合强度的影响</p>
+                            </Link>
+                            <Link to={`/system/article?uid=bad898c3-f37b-4e5c-bf54-e16e81f40771=P1`} >
+                                <p>热、力耦合作用下热障涂层的失效机制</p>
+                            </Link>
+
+
+
+                            {/* {
                                 this.props.recom1_datalist.map(value => {
                                     return(<div>
                                         <Link to={`/system/article?uid=${value.uid}=${this.props.article_type}`} >
@@ -88,7 +115,7 @@ class Home extends Component{
 
                                     </div>)
                                 })
-                            }
+                            }*/}
                         </Card>
                     </Col>
                     {/*<Col span={8}>
@@ -96,17 +123,28 @@ class Home extends Component{
                     </Col>*/}
                     <Col span={12}>
                         <Card title="热搜排名" bordered={false}>
-                            {
-                                this.props.recom2_datalist.map(value => {
-                                    return(<div>
-                                        <Link to={`/system/article?uid=${value.uid}=${this.props.article_type}`} >
-                                            <p> {value.name}</p>
-                                        </Link>
+                            <Link to={`/system/article?uid=bad898c3-f37b-4e5c-bf54-e16e81f40771=P1`} >
+                                <p>热、力耦合作用下热障涂层的失效机制</p>
+                            </Link>
+                            <Link to={`/system/article?uid=a6f5b6e9-6cf2-47ab-b161-5d624365fab7=P1`} >
+                                <p>乌克兰巴顿焊接研究所的电子束物理气相沉积技术 </p>
+                            </Link>
+                            <Link to={`/system/article?uid=ba848e02-267b-4459-9cea-eaaaab423f82=P1`} >
+                                <p>磁控形状记忆合金Ni2MnGa的制备与性能研究</p>
+                            </Link>
+                            <Link to={`/system/article?uid=9567dd69-ab95-486f-88fb-6a58be440b0a=P1`} >
+                            <p>特种功能材料中的固态相变及应用</p>
+                            </Link>
+                            <Link to={`/system/article?uid=14fadd5b-15e9-4c5b-b889-99ed52adeda3=P1`} >
+                                <p>TiNiMo形状记忆合金的相变、形状记忆效应与力学性能研究</p>
+                            </Link>
+                            <Link to={`/system/article?uid=03d0d191-af1e-4962-a45c-7608fd9249e9=P1`} >
+                                <p>基板温度对EB-PVD梯度热障涂层的微观结构和性能的影响</p>
+                            </Link>
+                            <Link to={`/system/article?uid=8f2576de-43e1-4d71-836f-797037e50c89=P1`} >
+                                <p>表面强化对EB-PVD热障涂层的高温氧化性能及结合强度的影响</p>
+                            </Link>
 
-
-                                    </div>)
-                                })
-                            }
 
                         </Card>
 
